db8bfbe4edbcbf64f5f8747ef59d23a6528630bb
wolam <wilhelm@estudiantec.cr>
2021-11-02 14:49:08 -0600

    Preview of the top_down approach

--- contenedor.py
+++ contenedor.py
@@ -9,10 +9,10 @@ parser = argparse.ArgumentParser(
     formatter_class=argparse.RawDescriptionHelpFormatter,
     description=textwrap.dedent('''
     NAME
-        Python 3 brute_force_knapSack Problem Analysis
+        Python 3 Knapsack Problem Analysis
     SYNOPSIS
         contenedor.py [-h] algorithm -a file.txt
-        contenedor.py [-h] algorithm -p W N weights benefits iterations
+        contenedor.py [-h] algorithm -p capacity N weights benefits iterations
     DESCRIPTION
     METHOD
         Desired algorithm to solve the problem.
@@ -23,44 +23,49 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument('algorithm', metavar='algorithm', type=int,
                     help=textwrap.dedent('''
-                    Algorithm to solve the brute_force_knapSack problem'''))
+                    Algorithm to solve the knapsack problem'''))
 
 # parser.add_argument('-a', metavar=', -a', help='Argument to run the program with data on txt file')
 
 parser.add_argument('file', metavar='file.txt', type=argparse.FileType("r"),
-                    help='Text file with the brute_force_knapSack problem in the correct format')
+                    help='Text file with the knapsack problem in the correct format')
 
 parser.add_argument('iterations', metavar='iterations', type=int,
                     help='Number of times the algorithm should be run')
 
 args = parser.parse_args()
 
-def parse_file(file=args.file) -> list:
-    arr = []
-    for line in file:
-        data = line.replace('\n', "").split(",")
-        arr.append(list(map(int, data)))
-    args.file.close()
-    return arr
 
-def brute_force_knapSack(W, weights, benefits, n, elements_used):
+def read_file(file=args.file) -> tuple:
+    capacity = int(file.readline())
+    weights, benefits = [], []
+    for line in file:
+        capacity, b = map(int, line.split(","))
+        weights.append(capacity)
+        benefits.append(b)
+    args.file.close()
+    return capacity, weights, benefits
+
+
+def brute_force_knapsack(capacity: int, n: int, weights: list, benefits: list, elements_used: list):
     # hold the elements of the two combinations  
     case_1 = []
     case_2 = []
- 
+
     # Base Case
-    if n == 0 or W <= 0:
+    if n == 0 or capacity <= 0:
         return 0
- 
-    # each item's weight can't be more than W
-    if (weights[n-1] > W):
-        return brute_force_knapSack(W, weights, benefits, n-1, case_2)
+
+    # each item's weight can't be more than capacity
+    if weights[n - 1] > capacity:
+        return brute_force_knapsack(capacity, weights, benefits, n - 1, case_2)
 
     else:
-        case_1.append(n) # put the element in case_1 and pass case_1 
-        combination_1 = benefits[n-1] + brute_force_knapSack(W-weights[n-1], weights, benefits, n-1, case_1)
+        case_1.append(n)  # put the element in case_1 and pass case_1 
+        combination_1 = benefits[n - 1] + brute_force_knapsack(capacity - weights[n - 1],
+                                                               weights, benefits, n - 1, case_1)
         # don't put anything in case_2 but pass case_2
-        combination_2 = brute_force_knapSack(W, weights, benefits, n-1, case_2)
+        combination_2 = brute_force_knapsack(capacity, weights, benefits, n - 1, case_2)
         # get the max situation between the two combinations
         max_value = max(combination_1, combination_2)
         # if this situacion occurs then nth item is included
@@ -70,16 +75,34 @@ def brute_force_knapSack(W, weights, benefits, n, elements_used):
             elements_used += case_2
         return max_value
 
+
+def top_down_knapsack(capacity: int, weights: list, benefits: list, memo: list, current: int):
+    if capacity <= 0 or current >= len(benefits):
+        return 0
+
+    if memo[current][capacity] is not None:
+        return memo[current][capacity]
+
+    item_taken = 0
+    if weights[current] <= capacity:
+        item_taken = benefits[current] + top_down_knapsack(memo, weights, benefits,
+                                                           capacity - weights[current], current + 1)
+    item_not_taken = top_down_knapsack(memo, weights, benefits, capacity, current + 1)
+
+    memo[current][capacity] = max(item_not_taken, item_taken)
+    return memo[current][capacity]
+
+
 def main():
-    data = parse_file()
-    W = data[0][0]
-    weights = [row[0] for row in data[1:]]
-    benefits = [row[1] for row in data[1:]]
+    capacity, weights, benefits = read_file()
     n = len(benefits)
     elements_used = []
-    print(brute_force_knapSack(W, weights, benefits, n, elements_used))
+    # memo = [[0 * len(benefits)] * len(weights)]
+    # print(top_down_knapsack(capacity, weights, benefits, memo, 0)
+    print(brute_force_knapsack(capacity, n, weights, benefits, elements_used))
     elements_used.sort()
     print(elements_used)
+
 
 if __name__ == "__main__":
     main()
